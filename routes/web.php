abeb23395a0c04f875a13ca01a828dfd090f5a58
Akil <akilpatel0572@gmail.com>
2024-09-11 11:38:38 +0530

    crud

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::controller( App\Http\Controllers\CategoryController::class)->group(function () {
+    Route::get('categories', 'index');
+    Route::get('categories/create', 'create');
+    Route::post('categories/create', 'store');
+    Route::get('categories/{id}/edit', 'edit');
+    Route::put('categories/{id}/edit', 'update');
+    Route::get('categories/{id}/delete', 'destroy');
+});
